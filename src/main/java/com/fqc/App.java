e4ad8b0af05ad1d090b1da0ab432ef1725b222a6
fqc <337940626@qq.com>
2017-02-12 16:32:39 +0800

    初始化工程

---
+++ src/main/java/com/fqc/App.java
@@ -0,0 +1,13 @@
+package com.fqc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
